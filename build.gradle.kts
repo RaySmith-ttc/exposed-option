fee26ee679be7817d2ae998cd77b90d87662da80
Ray Smith <raysmith.ttcreate@gmail.com>
2024-04-19 22:12:09 +0600

    build: add maven central publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.9.10"
-    id("maven-publish")
+    `maven-publish`
+    signing
+    id("com.gradleup.nmcp") version "0.0.7"
 }
 
 group = "ru.raysmith"
@@ -15,24 +17,6 @@ repositories {
         credentials {
             username = System.getenv("GIT_USERNAME")
             password = System.getenv("GIT_TOKEN_READ")
-        }
-    }
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/raysmith-ttc/exposed-option")
-            credentials {
-                username = System.getenv("GIT_USERNAME")
-                password = System.getenv("GIT_TOKEN_PUBLISH")
-            }
-        }
-    }
-    publications {
-        register<MavenPublication>("gpr") {
-            from(components["java"])
         }
     }
 }
@@ -64,4 +48,70 @@ tasks {
     test {
         useJUnitPlatform()
     }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            artifactId = project.name
+            groupId = project.group.toString()
+            version = project.version.toString()
+            from(components["java"])
+            pom {
+                packaging = "jar"
+                name.set("Google")
+                url.set("https://github.com/RaySmith-ttc/google")
+                description.set("DSL wrappers for java google libs")
+
+
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:https://github.com/RaySmith-ttc/google.git")
+                    developerConnection.set("scm:git@github.com:RaySmith-ttc/google.git")
+                    url.set("https://github.com/RaySmith-ttc/google")
+                }
+
+                developers {
+                    developer {
+                        id.set("RaySmith-ttc")
+                        name.set("Ray Smith")
+                        email.set("raysmith.ttcreate@gmail.com")
+                    }
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "OSSRH"
+            val releasesUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().matches(".*(SNAPSHOT|rc.\\d+)".toRegex())) snapshotsUrl else releasesUrl
+            credentials {
+                username = System.getenv("SONATYPE_USER")
+                password = System.getenv("SONATYPE_PASS")
+            }
+        }
+    }
+}
+
+nmcp {
+    publish("release") {
+        username.set(System.getenv("CENTRAL_SONATYPE_USER"))
+        password.set(System.getenv("CENTRAL_SONATYPE_PASS"))
+        publicationType.set("USER_MANAGED")
+        publicationType.set("AUTOMATIC")
+    }
+}
+
+signing {
+    sign(configurations.archives.get())
+    sign(publishing.publications["release"])
 }
